0854e2ea615a55ed536e7400ed52748deecd7342
Alucaro <alucaro29a@hotmail.com>
2022-08-24 11:21:27 -0500

    connecting app js on index js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//Importacion
+import React from "react";
+
+//Generacion del componente
+//corresponde al nombre del archivo por buena practica
+const App = () => {
+    //Lo que ejecuta la funcion
+    return <h1> Hola Mundo (app)</h1>
+}
+
+//Exportacion
+export default App //El nombre del componente
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'//; is optional
+import ReactDOM from 'react-dom'
+//importar el componente
+import App from './App'
+
+//utilizar el componente
+ReactDOM.render(<App></App>,
+    document.getElementById('root'))
